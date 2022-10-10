17b749432be6e0c2bae6a90e7deed7f7391efd2a
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-10-10 17:02:42 -0400

    first database! 😃

---
+++ index.js
@@ -0,0 +1,41 @@
+import { initializeApp, cert } from "firebase-admin/app"; 
+import { getFirestore } from "firebase-admin/firestore";
+
+// import our credentials (serviceAccount)
+import serviceAccount from './serviceAccount.js';
+
+// connect to our firebase project using those credentials
+initializeApp({
+  credential: cert(serviceAccount)
+});
+
+// connect to our firestore database
+const db = getFirestore();
+// do everything above for each new firestore DB
+
+
+// define a new videogame:
+const newGame = {
+  title: 'Frogger',
+  rated: 'E',
+  genre: 'Arcade',
+  released: 1981,
+}
+
+// create a doc inside a collection
+db.collection('games').add(newGame) //go to db, go to games collection, add this data to new doc
+  // if ok, console log the doc id
+  .then(doc => console.log('Game created: ', doc.id)) //when done, console log that game was created with new document id
+  // if not, console the error
+  .catch(console.error); //when done, if an error is caught it will console log an error
+  // .catch(err => console.error(err)) does same as above
+
+// get all games
+db.collection('games').get()
+  // reshape the collection
+  .then(collection => {
+    collection.docs.forEach(doc => {
+      console.log(doc.id, doc.data())
+    })
+  })
+  .catch(console.error);
